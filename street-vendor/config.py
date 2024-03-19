d8315cafb51b4d8a2b0319b3929768b89ced9bee
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-03-20 00:23:03 +0200

    connecting the app to mysql testing database

--- street-vendor/config.py
+++ street-vendor/config.py
@@ -11,3 +11,4 @@ class Config:
        Configuration class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
