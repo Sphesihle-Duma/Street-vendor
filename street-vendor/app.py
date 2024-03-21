23165909e9e27eae5fd315d7a7cdde04b41dcc68
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-03-21 13:27:03 +0200

    creating application context for flask shell

--- street-vendor/app.py
+++ street-vendor/app.py
@@ -2,7 +2,17 @@
 '''
    The main application
 '''
-from mainapp import app
+from mainapp import app, db
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from mainapp.models import Street, Space
+
+@app.shell_context_processor
+def make_shell_context():
+    '''
+       Setting flask shell context
+    '''
+    return {'sa': sa, 'so': so, 'db': db, 'Street': Street, 'Space': Space}
 
 
 if __name__ == '__main__':
--- street-vendor/mainapp/models.py
+++ street-vendor/mainapp/models.py
@@ -14,9 +14,27 @@ class Street(db.Model):
     street_id: so.Mapped[int] = so.mapped_column(primary_key=True)
     street_name: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
                                                    unique=True)
+    spaces: so.WriteOnlyMapped['Space'] = so.relationship(back_populates='street')
 
     def __repr__(self):
         '''
            String represantation of the street object
         '''
         return '<User {}>'.format(self.street_name)
+
+
+class Space(db.Model):
+    '''
+        A model representing a space table
+    '''
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    space_number: so.Mapped[int] = so.mapped_column(unique=True)
+    street_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Street.street_id),
+                                                 index=True)
+    street: so.Mapped[Street] = so.relationship(back_populates='spaces')
+
+    def __repr__(self):
+        '''
+           string represention of the space object
+        '''
+        return '<Space {}>'.format(self.space_number)
